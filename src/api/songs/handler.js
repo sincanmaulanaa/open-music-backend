773ec0fb870cd6e515ae9cd537d964a39dec386b
sincanmaulanaa <sincanmaulanaa@gmail.com>
2023-08-02 03:01:45 +0700

    Implement find songs by title and performer

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -40,8 +40,9 @@ class SongsHandler {
     return response;
   }
 
-  async getSongsHandler() {
-    const songs = await this._service.getSongs();
+  async getSongsHandler(request, h) {
+    const { title, performer } = request.query;
+    const songs = await this._service.getSongs(title, performer);
     return {
       status: 'success',
       data: {
