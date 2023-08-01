3597b6aefdd3a1fec3f438751669d575fb058824
sincanmaulanaa <sincanmaulanaa@gmail.com>
2023-08-02 03:02:04 +0700

    Implement find songs by title and performer

--- src/services/SongsService.js
+++ src/services/SongsService.js
@@ -26,10 +26,28 @@ class SongsService {
     return result.rows[0].id;
   }
 
-  async getSongs() {
-    const result = await this._pool.query(
-      'SELECT id, title, performer FROM songs'
-    );
+  async getSongs(title, performer) {
+    let query = '';
+
+    if (title && performer) {
+      query = {
+        text: 'SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE $1 AND LOWER(performer) LIKE $2',
+        values: [`%${title.toLowerCase()}%`, `%${performer.toLowerCase()}%`],
+      };
+    } else if (title) {
+      query = {
+        text: 'SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE $1',
+        values: [`%${title.toLowerCase()}%`],
+      };
+    } else if (performer) {
+      query = {
+        text: 'SELECT id, title, performer FROM songs WHERE LOWER(performer) LIKE $1',
+        values: [`%${performer.toLowerCase()}%`],
+      };
+    } else {
+      query = 'SELECT id, title, performer FROM songs';
+    }
+    const result = await this._pool.query(query);
     return result.rows.map(mapDBToModel);
   }
 
